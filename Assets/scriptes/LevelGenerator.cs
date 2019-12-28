6dab046165f099e83a9b45fcbef0996dd4fe97c3
doha <57834946+hero3131@users.noreply.github.com>
2019-12-28 13:51:52 +0100

    screenshot

---
+++ Assets/scriptes/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject platformPrefab;
+
+    public int numberOfPlatforms = 200;
+    public float levelWidth = 3f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+        
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
